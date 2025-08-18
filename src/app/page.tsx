c44bdd04c4fdbbd97faf007f373bc4ac9de318c4
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-08-18 10:08:56 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
+import Image from "next/image";
 import Header from "@/components/common/header";
+import { db } from "@/db";
+import ProductList from "@/components/common/product-list";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
