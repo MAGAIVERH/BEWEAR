bae008b85b2782c893415a7edb9dcee6d7d8a131
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-08-20 09:48:49 -0300

    feat: add product page

---
+++ src/app/product-variant/[slug]/page.tsx
@@ -0,0 +1,103 @@
+import Header from "@/components/common/header";
+import { db } from "@/db";
+import { productTable, productVariantTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import { formatCentsToBRL } from "@/helpers/money";
+import { Button } from "@/components/ui/button";
+import ProductList from "@/components/common/product-list";
+import Footer from "@/components/common/footer";
+import VariantSelector from "./components/variant-selector";
+import QuantitySeletor from "./components/quantity-selector";
+
+interface ProductVariantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
+  const { slug } = await params;
+  const productVariant = await db.query.productVariantTable.findFirst({
+    where: eq(productVariantTable.slug, slug),
+    with: {
+      product: {
+        with: {
+          variants: true,
+        },
+      },
+    },
+  });
+  if (!productVariant) {
+    return notFound();
+  }
+
+  const likelyProducts = await db.query.productTable.findMany({
+    where: eq(productTable.categoryId, productVariant.product.categoryId),
+    with: { variants: true },
+  });
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col space-y-6">
+        {/* {image} */}
+        {/* {para obter um crotole melhor do tamanho da imagem , usar uma div e colocar a imagem dentro usando o fill com o object cover ou o contain} */}
+        {/* <div className="relative h-[380px] w-full rounded-3xl"> */}
+        <Image
+          src={productVariant.imageUrl}
+          alt={productVariant.name}
+          // fill
+          // className="object-cover"
+          sizes="100vw"
+          width={0}
+          height={0}
+          className="h-auto w-full"
+        />
+
+        <div className="px-5">
+          {/* {variants} */}
+          <VariantSelector
+            selectedVariantSlug={productVariant.slug}
+            variants={productVariant.product.variants}
+          />
+        </div>
+
+        <div className="px-5">
+          {/* {Descricao} */}
+          <h2 className="text-lg font-semibold">
+            {productVariant.product.name}
+          </h2>
+          <h3 className="text-muted-foreground text-sm">
+            {productVariant.name}
+          </h3>
+          <h3 className="text-lg font-semibold">
+            {formatCentsToBRL(productVariant.priceInCents)}
+          </h3>
+        </div>
+        <div className="px-5">
+          {/* {Quantidade} */}
+          <QuantitySeletor />
+        </div>
+
+        <div className="flex flex-col space-y-4 px-5">
+          {/* {botoes} */}
+          <Button className="rounded-full" size="lg" variant="outline">
+            Adicionar a sacola
+          </Button>
+          <Button className="rounded-full" size="lg">
+            Comprar agora
+          </Button>
+        </div>
+
+        <div className="px-5">
+          <p className="text-sm">{productVariant.product.description}</p>
+        </div>
+
+        <ProductList title="Talvez você goste" products={likelyProducts} />
+
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default ProductVariantPage;
