af0fc8f1268bb761041589baa6a1b0eff512e7ce
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-09-05 18:06:57 -0300

    fix: fix file header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -23,18 +23,16 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between p-5">
       <Link href="/">
-        <Image src="/logo.svg" alt="BEWEAR" width={100} height={26} />
+        <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
 
       <div className="flex items-center gap-3">
-        <Cart />
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
               <MenuIcon />
             </Button>
           </SheetTrigger>
-
           <SheetContent>
             <SheetHeader>
               <SheetTitle>Menu</SheetTitle>
@@ -42,13 +40,12 @@ const Header = () => {
             <div className="px-5">
               {session?.user ? (
                 <>
-                  <div className="flex justify-between">
+                  <div className="flex justify-between space-y-6">
                     <div className="flex items-center gap-3">
                       <Avatar>
                         <AvatarImage
                           src={session?.user?.image as string | undefined}
                         />
-
                         <AvatarFallback>
                           {session?.user?.name?.split(" ")?.[0]?.[0]}
                           {session?.user?.name?.split(" ")?.[1]?.[0]}
@@ -62,7 +59,6 @@ const Header = () => {
                         </span>
                       </div>
                     </div>
-
                     <Button
                       variant="outline"
                       size="icon"
@@ -73,23 +69,21 @@ const Header = () => {
                   </div>
                 </>
               ) : (
-                <>
-                  <div className="flex items-center justify-between">
-                    <h2 className="font-semibold">Olá. Faça seu login!</h2>
-                    <Button size="icon" asChild variant="outline">
-                      <Link href="/authentication">
-                        <LogInIcon />
-                      </Link>
-                    </Button>
-                  </div>
-                </>
+                <div className="flex items-center justify-between">
+                  <h2 className="font-semibold">Olá. Faça seu login!</h2>
+                  <Button size="icon" asChild variant="outline">
+                    <Link href="/authentication">
+                      <LogInIcon />
+                    </Link>
+                  </Button>
+                </div>
               )}
             </div>
           </SheetContent>
         </Sheet>
+        <Cart />
       </div>
     </header>
   );
 };
-
 export default Header;
